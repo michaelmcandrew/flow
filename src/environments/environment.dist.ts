7c5c43f639183cae2685e5a70d75b32df647c2e3
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-07-24 11:53:02 +0100

    ignore sensitive env variables

---
+++ src/environments/environment.dist.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  restApiBaseUrl: 'http://dmaster.buildkit:8080/sites/all/modules/civicrm/extern/rest.php',
+  siteKey: 'xxxx',
+  apiKey: 'xxx'
+};
